69a50e83a6176a40aac2dd8e27bc784e95d13a63
Zac M-W <contact@zac.nz>
2025-01-10 12:50:57 +0000

    Initial version

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.postcss';
+	import Icon from '@iconify/svelte';
+	import { page } from '$app/state';
+	let { children } = $props();
+</script>
+
+<div class="flex h-screen flex-col">
+	<div class="wrapper grow">
+		{@render children()}
+	</div>
+
+	<div class="dock">
+		<a class:dock-active={page.route.id === '/'} href="/">
+			<Icon icon="material-symbols:home-outline-rounded" />
+			<span class="dock-label">Home</span>
+		</a>
+
+		<a class:dock-active={page.route.id === '/map'} href="/map">
+			<Icon icon="material-symbols:map-outline-rounded" />
+			<span class="dock-label">Map</span>
+		</a>
+
+		<a class:dock-active={page.route.id === '/info'} href="/info">
+			<Icon icon="material-symbols:info-outline-rounded" />
+			<span class="dock-label">Info</span>
+		</a>
+	</div>
+</div>
---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { Map, Geocoder, Marker, controls } from '@beyonk/svelte-mapbox';
+	import { env } from '$env/dynamic/public';
+	const { GeolocateControl, NavigationControl, ScaleControl } = controls;
+
+	let mapComponent = $state();
+</script>
+
+<div class="h-full">
+	<Map
+		class="h-full"
+		accessToken={env.PUBLIC_MAPBOX_TOKEN}
+		bind:this={mapComponent}
+		on:recentre={(e) => console.log(e.detail.center.lat, e.detail.center.lng)}
+		options={{ scrollZoom: false }}
+	>
+		<Marker
+			lat={51.497799}
+			lng={-0.17922}
+			color="rgb(255,255,255)"
+			label="some marker label"
+			popupClassName="class-name"
+		/> // built in Marker component
+		<NavigationControl />
+		<GeolocateControl options={{ some: 'control-option' }} />
+		<ScaleControl />
+	</Map>
+</div>
