f39d0050195b25802fccab5c5535493942f74d97
Zac M-W <contact@zac.nz>
2025-01-13 06:51:49 +0000

    Filter and sort results

--- src/lib/directus.server.svelte.ts
+++ src/lib/directus.server.svelte.ts
@@ -4,6 +4,7 @@ import { env as pubEnv } from '$env/dynamic/public';
 import { browser } from '$app/environment';
 import { Tag, TagList, type TagData } from './tags.svelte';
 import { appState } from './appState.svelte';
+import { _ } from '$env/static/private';
 
 const client = createDirectus(env.directus_url).with(authentication()).with(rest());
 client.setToken(env.directus_token);
@@ -13,7 +14,13 @@ export const getTags = async () => {
 		(
 			(await client.request(
 				readItems('NFC_Tags', {
-					fields: ['id', 'status', 'sort', 'Name', 'Hint', 'hint_image', 'location']
+					fields: ['id', 'status', 'sort', 'Name', 'Hint', 'hint_image', 'location'],
+					sort: 'sort',
+					filter: {
+						status: {
+							_eq: 'published'
+						}
+					}
 				})
 			)) as TagData[]
 		).map((t) => {
