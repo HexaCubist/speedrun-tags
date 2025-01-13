a6e4a70174b8e912e7d9cd9e322d340959dd247f
Zac M-W <contact@zac.nz>
2025-01-13 06:52:02 +0000

    Improve dashboard UI

--- src/lib/components/dashboard/dashboard.svelte
+++ src/lib/components/dashboard/dashboard.svelte
@@ -30,28 +30,41 @@
 		<div class="stat flex shrink grow basis-20 items-center justify-center">
 			<div class="stat-value">
 				<div
-					class="radial-progress bg-primary text-primary-content border-primary border-4"
+					class="radial-progress bg-primary text-primary-content border-primary border-4 text-base"
 					style:--value={percentFound * 100}
 					style:--size="4rem"
 					role="progressbar"
 				>
-					{Math.round(percentFound)}%
+					{Math.round(percentFound * 100)}%
 				</div>
 			</div>
 		</div>
 		{#if tags.lastTag}
 			<div class="stat shrink grow basis-20">
 				<div class="stat-title">Challenge Time</div>
-				<div class="stat-value">{tags.lastTag?.sinceFound}</div>
-				<div class="stat-desc">Tag{tags.foundTags.length === 1 ? '' : 's'}</div>
+				<div class="stat-value">{tags.foundTags[0]?.sinceFound}</div>
 			</div>
 		{/if}
 	</div>
 
-	<div class="h-80">
+	<div class="rounded-box h-80 overflow-clip shadow-md">
 		<Map {tags} />
 	</div>
 
+	<div class="stats bg-primary text-base-100 flex flex-wrap shadow">
+		{#if tags.nextTag}
+			<a class="stat shrink grow basis-full" href={tags.nextTag.link}>
+				<div class="stat-title text-base-100">Next Tag Hint (tap to open directions)</div>
+				<div class="stat-value text-left text-lg text-wrap">
+					{#if nextTag?.data.Hint}
+						{@html tags.nextTag.data.Hint}
+					{:else}
+						No hint... Good luck!
+					{/if}
+				</div>
+			</a>
+		{/if}
+	</div>
 	<div class="flex flex-wrap gap-2">
 		{#if dev}
 			<button onclick={() => tags.nextTag?.find()} class="btn btn-primary"
@@ -61,9 +74,6 @@
 			<button onclick={() => tags.lastTag?.unfind()} class="btn btn-primary"
 				>Unfind next tag admin</button
 			>
-		{/if}
-		{#if tags.nextTag}
-			<a href={tags.nextTag?.link} class="btn btn-primary">Open next tag in Google Maps</a>
 		{/if}
 	</div>
 </div>
